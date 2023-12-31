08e84f3ee2192268297d5b0f78bf5dfda37a5298
CodesVenkat <155305281+CodesVenkat@users.noreply.github.com>
2023-12-31 20:03:45 +0530

    Update Get_post_from_api.js
    Get & Post Api

--- Get_post_from_api.js
+++ Get_post_from_api.js
@@ -8,28 +8,40 @@ const [data, setData] = useState([]);
 
 // TO get an api from Json using apiService file
 
-  const getApi = () => {
-    apiService("Products", "", "unauthget")
-      .then((result) => {
-        setData(result.data);
-      })
-      .catch((err) => {})
-      .finally(() => {
-      });
-  };
+// Function to fetch data from the API
+const getApi = () => {
+  apiService("Products", "", "unauthget")
+    .then((result) => {
+      // When the API request is successful, update the data state
+      setData(result.data);
+    })
+    .catch((err) => {
+      // Handle any errors that occur during the API request
+    })
+    .finally(() => {
+      // This block will be executed regardless of success or failure
+    });
+};
+
 //  TO post From API
 
-  const onSubmit = (data) => {
-    apiService("Products", data, "unauthpost")
-      .then((result) => {
-        getApi();
-      })
-      .catch((err) => {
-        console.log(err);
-      })
-      .finally(() => {
-      });
-  };
+// Function to submit data to the API on form submission.
+const onSubmit = (data) => {
+  // Making a POST request to the "Products" endpoint with the form data
+  apiService("Products", data, "unauthpost")
+    .then((result) => {
+      // After a successful post, fetch the updated data from the API
+      getApi();
+    })
+    .catch((err) => {
+      // Log any errors that occur during the API post request
+      console.log(err);
+    })
+    .finally(() => {
+      // This block will be executed regardless of success or failure
+    });
+};
+
 
 //  TO submit the data using Forms
 
