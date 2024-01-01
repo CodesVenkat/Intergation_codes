08140b4cb2f4c4499e15d050a8a7ea376f3b327f
CodesVenkat <155305281+CodesVenkat@users.noreply.github.com>
2024-01-01 22:23:26 +0530

    Create grid_sizes.js
     SIZES

---
+++ grid_sizes.js
@@ -0,0 +1,8 @@
+
+In Material-UI's Grid system, there are five predefined grid sizes that you can use:
+
+xs (Extra Small): This is for screens smaller than 600px.
+sm (Small): This is for screens equal to or larger than 600px.
+md (Medium): This is for screens equal to or larger than 960px.
+lg (Large): This is for screens equal to or larger than 1280px.
+xl (Extra Large): This is for screens equal to or larger than 1920px.
