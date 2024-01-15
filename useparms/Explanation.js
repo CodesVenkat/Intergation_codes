b2c4dccbf7221c647b4c57d9997c6cdddc6c9d7f
CodesVenkat <155305281+CodesVenkat@users.noreply.github.com>
2024-01-15 19:53:49 +0530

    Create Explanation.js
    Explanation of useparams

---
+++ useparms/Explanation.js
@@ -0,0 +1,10 @@
+1. Using location to get current data
+
+ How to use ==> 
+useNavigate() = navigate ( "pathname" ,{state: we can send the data})    in that pathname where we can get the data by using uselocation method 
+
+2. Using useParams to get current I'd of the data and  we can filter it after
+
+How to use => 
+useNavigate() = navigate("pathname when using use params we want I'd for example  "/card/:id" )
+Using I'd we can filter the array and get the exact I'd of the card (note: use params I'd is string)
