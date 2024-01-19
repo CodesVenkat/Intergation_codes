288f5179cc3f78647fca20c0577bf9275ddaf659
CodesVenkat <155305281+CodesVenkat@users.noreply.github.com>
2024-01-19 09:41:35 +0530

    Update getelementbyid.js
    get the id by clicking the element

--- useparms/getelementbyid.js
+++ useparms/getelementbyid.js
@@ -132,10 +132,10 @@ import React from 'react'
 import { useParams } from 'react-router-dom'
 
 const DisplayProduct = () => {    
-    const{id}=useParams();
+    const{productId}=useParams();
   return (
     <div>
-        <h1>{`${id}`}</h1>
+        <h1>{`${productId}`}</h1>
     </div>
   )
 }
@@ -159,7 +159,7 @@ function App() {
     
     <Routes>
       <Route path="/DisplayProduct" element={<DisplayProduct/>} />
-      <Route path="/DisplayProduct/:id" element={<DisplayProduct/>} />
+      <Route path="/DisplayProduct/:productId" element={<DisplayProduct/>} />
     </Routes>
     </LoadingContext>
     
