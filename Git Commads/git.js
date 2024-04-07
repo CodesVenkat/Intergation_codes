f9ca4f5aedb4f745d1157fc1c484a294e21594b7
CodesVenkat <155305281+CodesVenkat@users.noreply.github.com>
2024-04-07 22:56:46 +0530

    Update git.js

--- Git Commads/git.js
+++ Git Commads/git.js
@@ -8,3 +8,9 @@ git pull origin cmarf-claimant
 to create new branch
 
 git checkout -b cmarf-claimant-venkat
+
+
+if remote already exists error found in github
+
+
+git remote set-url origin https://github.com/Jothi123venkat/actors.git
